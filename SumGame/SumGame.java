511abfcb7cbe8365958736e2294bc2c7cf6d4012
Pradeep Medikonda <prade@MacBook-Air.local>
2026-08-23 20:56:03 +0530

    SumGame on 22/08

---
+++ SumGame/SumGame.java
@@ -0,0 +1,26 @@
+public class SumGame {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int[] left = get(num.substring(0,n/2));
+        int[] right = get(num.substring(n/2,n));
+        int n0= left[0];
+        int q0 = left[1];
+        int n1 = right[0];
+        int q1 = right[1];
+        return  (q0 + q1) % 2 == 1 || n0 - n1 != ((q1 - q0) * 9) / 2;
+        
+    }
+
+    private int[] get(String s){
+        int nn = 0;
+        int qq = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '?'){
+                qq++;
+            } else {
+                nn += ch - '0';
+            }
+        }
+        return new int[] { nn , qq };
+    }
+}
